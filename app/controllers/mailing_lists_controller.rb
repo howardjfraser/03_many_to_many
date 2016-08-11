e13f458d39d76c6202fbc37ad0eda2374f7c18de
Howard Fraser <howardjfraser@gmail.com>
2016-08-11 10:45:40 +1000

    Add remaining actions.

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -7,9 +7,42 @@ class MailingListsController < ApplicationController
     @mailing_list = MailingList.find params[:id]
   end
 
+  def new
+    @mailing_list = MailingList.new
+  end
+
+  def create
+    @mailing_list = MailingList.new mailing_list_params
+    if @mailing_list.save
+      redirect_to @mailing_list, notice: "#{@mailing_list.name} mailing list has been created"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @mailing_list = MailingList.find params[:id]
+  end
+
+  def update
+    @mailing_list = MailingList.find params[:id]
+    @mailing_list.update mailing_list_params
+    if @mailing_list.save
+      redirect_to @mailing_list, notice: "#{@mailing_list.name} mailing list has been updated"
+    else
+      render 'update'
+    end
+  end
+
   def destroy
     @mailing_list = MailingList.find params[:id]
     @mailing_list.destroy
     redirect_to mailing_lists_path, notice: "#{@mailing_list.name} mailing list has been deleted"
+  end
+
+  private
+
+  def mailing_list_params
+    params.require(:mailing_list).permit(:name, :email)
   end
 end
--- test/controllers/mailing_lists_controller_test.rb
+++ test/controllers/mailing_lists_controller_test.rb
@@ -9,14 +9,61 @@ class MailingListsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'h1', 'Mailing lists'
     assert_select '#mailing_lists', /Development.*Marketing.*Support/m
-
-    # TODO: check new link
+    assert_select '.btn', 'Add New'
   end
 
   test 'show' do
     get mailing_list_path @mailing_list
     assert_response :success
     assert_select 'h1', @mailing_list.name
+  end
+
+  test 'new' do
+    get new_mailing_list_path
+    assert_response :success
+    assert_select 'h1', 'New'
+  end
+
+  test 'valid create' do
+    name = sample_string
+    assert_difference 'MailingList.count', 1 do
+      post mailing_lists_path, params: { mailing_list: {name: name, email: sample_string}}
+    end
+    assert_response :redirect
+    follow_redirect!
+    assert_select 'h1', name
+    assert_select '.flash', "#{name} mailing list has been created"
+  end
+
+  test 'invalid create' do
+    assert_no_difference 'MailingList.count' do
+      post mailing_lists_path, params: {mailing_list: { name: '', email: ''}}
+    end
+    assert_response :success
+    assert_select 'h1', 'New'
+    assert_select '.field_with_errors label', 'Name can’t be blank'
+  end
+
+  test 'edit' do
+    get edit_mailing_list_path @mailing_list
+    assert_response :success
+    assert_select 'h1', 'Edit'
+  end
+
+  test 'valid update' do
+    new_name = sample_string
+    patch mailing_list_path @mailing_list, params: {mailing_list: {name: new_name}}
+    assert_response :redirect
+    follow_redirect!
+    assert_select 'h1', new_name
+    assert_select '.flash', "#{new_name} mailing list has been updated"
+  end
+
+  test 'invalid update' do
+    patch mailing_list_path @mailing_list, params: {mailing_list: {name: ''}}
+    assert_response :success
+    assert_select 'h1', 'New'
+    assert_select '.field_with_errors label', 'Name can’t be blank'
   end
 
   test 'delete' do
